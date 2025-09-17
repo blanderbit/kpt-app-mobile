89cf0e5348400b281e75f3b5781957e51e9d57e1
danil <d.utyuzh@codeska.com>
2025-09-17 15:59:31 +0300

    main - testing google auth

--- src/features/auth/screens/test3.tsx
+++ src/features/auth/screens/test3.tsx
@@ -1,6 +1,6 @@
 // App.tsx
 import React, { useEffect, useState } from "react";
-import {Button, View, Text, Alert, Linking} from "react-native";
+import {Button, View, Text, Alert, Linking, Platform} from "react-native";
 import * as Google from "expo-auth-session/providers/google";
 import * as AuthSession from "expo-auth-session";
 import * as WebBrowser from "expo-web-browser";
@@ -55,24 +55,34 @@ const redirectUri =
 export default function GoogleSignInButton() {
     // Настройка для iOS симулятора с Expo Go
     const [request, response, promptAsync] = Google.useAuthRequest({
-        webClientId: "78984716511-4fdfc5l4afgtag2hsbn3puvllmfc1v4h.apps.googleusercontent.com",
-        iosClientId: '78984716511-v1qg9hahti25vjvgmi72bcbov49fkcql.apps.googleusercontent.com',
+            // clientId: "78984716511-4fdfc5l4afgtag2hsbn3puvllmfc1v4h.apps.googleusercontent.com",
+            clientId: Platform.select({
+                ios: '1015243991043-tf5f0n2gsv8c8t28635v9rhv7j7rsg6a.apps.googleusercontent.com',
+                // android: "ANDROID_CLIENT_ID.apps.googleusercontent.com",
+                web: "78984716511-4fdfc5l4afgtag2hsbn3puvllmfc1v4h.apps.googleusercontent.com"
+            }),
+
+            responseType: "id_token",
+            scopes: ['profile', 'email'],
+            redirectUri: AuthSession.makeRedirectUri({ scheme: 'kpt-apple' }),
+        // webClientId: "78984716511-4fdfc5l4afgtag2hsbn3puvllmfc1v4h.apps.googleusercontent.com",
+        // iosClientId: '78984716511-v1qg9hahti25vjvgmi72bcbov49fkcql.apps.googleusercontent.com',
         // androidClientId: '78984716511-6u49n9quctr938qbdph6ffo4hi4redeb.apps.googleusercontent.com',
-        clientId: "78984716511-4fdfc5l4afgtag2hsbn3puvllmfc1v4h.apps.googleusercontent.com",
+        // clientId: Platform.select({
+        //     ios: '78984716511-v1qg9hahti25vjvgmi72bcbov49fkcql.apps.googleusercontent.com',
+        //     android: '78984716511-6u49n9quctr938qbdph6ffo4hi4redeb.apps.googleusercontent.com',
+        //     web: "78984716511-4fdfc5l4afgtag2hsbn3puvllmfc1v4h.apps.googleusercontent.com"
+        // }),
         // webClientId: "1015243991043-knrfog6fsga3u7pt8vstln4jbojfae77.apps.googleusercontent.com",
         // clientId: "1015243991043-knrfog6fsga3u7pt8vstln4jbojfae77.apps.googleusercontent.com",
         // iosClientId: "1015243991043-tf5f0n2gsv8c8t28635v9rhv7j7rsg6a.apps.googleusercontent.com",
         // androidClientId: '1015243991043-qecjkbib04vpdh8ra0455mojffg1iqva.apps.googleusercontent.com',
-        responseType: "id_token",
-        scopes: ['profile', 'email'],
+
         // Переключаемся между прокси и прямым редиректом
         // redirectUri: 'https://auth.expo.io/@wexis/KptApp/redirect'
         // redirectUri: 'https://auth.expo.io/@wexis/KptApp'
         // redirectUri: 'exp://192.168.0.102:8081/redirect'
-    },
-        {
-            scheme: 'test'
-        }
+    }
         // {
         // projectNameForProxy: "@wexis/KptApp",
         // native: "com.my.schema",
