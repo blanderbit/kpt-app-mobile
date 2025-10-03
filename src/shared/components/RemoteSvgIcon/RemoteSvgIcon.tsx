b8e431804f2d4d0b8f11076079a8d060c1d2160d
danil <d.utyuzh@codeska.com>
2025-10-03 11:47:07 +0300

    main - fix icon component

--- src/shared/components/RemoteSvgIcon/RemoteSvgIcon.tsx
+++ src/shared/components/RemoteSvgIcon/RemoteSvgIcon.tsx
@@ -1,4 +1,13 @@
 import React from "react";
 import { SvgXml } from "react-native-svg";
 
-export const RemoteSvg = ({ xml, size }: { xml: string, size?: number }) => <SvgXml xml={xml} height={size} width={size} />;
+export const RemoteSvg = ({ xml, size }: { xml: string, size?: number }) => {
+  const props: any = { xml };
+  
+  if (size !== undefined) {
+    props.height = size;
+    props.width = size;
+  }
+  
+  return <SvgXml {...props} />;
+};
